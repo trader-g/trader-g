cbf466cfaacc5be7b672ba5bd13471dfe9575b1d
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-03-03 11:05:51 +0200

    Inits spring application

---
+++ trader-g-api/src/main/java/com/traderg/tradergapi/TraderGApiApplication.java
@@ -0,0 +1,13 @@
+package com.traderg.tradergapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderGApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderGApiApplication.class, args);
+	}
+
+}
