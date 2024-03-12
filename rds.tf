5b91b0524bc5201100df24aed252567fa039f1c6
KeeganOReilly <159418048+KeeganOReilly@users.noreply.github.com>
2024-03-12 16:23:12 +0200

    BCC 4 - Architecture initial and updates. Deploys automagically on AWS (#3)
    * Added API, CICD and Flyway yamls
    
    * Added terraform files to deploy architecture on AWS | Added Github actions for API and other CI/CD | Added dockerfile | Added auth class and secrets on GitHub | Added runner from EC2 | Added (hopefully) all necessary architecture
    
    * Slight role-to-assume change
    
    * Changed GHActions to tagged PR auto-deploys | Updated .gitignore to ignore .class files | Updated rds.tf to run a smaller instance db
    
    * Moved Pom.xml
    
    * Removed mvn packages because job fails
    
    * changed image name
    
    * testing v1
    
    * Update: We have no jar file to copy yet, so can't build or publish
    
    * fixed image name v2
    
    * runner change
    
    * deploy not ready yet
    
    * Added Gerni's change requests
    
    * Damn, adding original pom.xml file back in

---
+++ rds.tf
@@ -0,0 +1,56 @@
+variable "db_password" {}
+variable "db_username" {}
+
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "default_vpc"
+  }
+}
+
+data "aws_availability_zones" "available_zones" {
+  
+}
+
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+}
+
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+}
+
+
+resource "aws_security_group" "allow_mssql" {
+  name        = "allow_mssql"
+
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_mssql"
+    owner = "keegan.oreilly@bbd.co.za"
+    created-using = "terraform"
+  }
+}
+
+resource "aws_db_instance" "beantrader-db" {
+  identifier             = "beantrader-db"
+  engine                 = "sqlserver-ex"
+  engine_version         = "14.00.3465.1.v1"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  publicly_accessible    = true
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.allow_mssql.id]
+  tags = {
+    owner         = "keegan.oreilly@bbd.co.za"
+    created-using = "terraform"
+  }
+}
