f2ac12e060e09f849910a3392f96c271a068f8a4
Thato Tladi <Thato.Tladi@bbd.co.za>
2024-03-12 16:06:09 +0200

    CLI changes to interface with backend

--- cli/src/main/java/com/traderg/cli/App.java
+++ cli/src/main/java/com/traderg/cli/App.java
@@ -1,22 +1,27 @@
 package com.traderg.cli;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.traderg.cli.Services.BackendService;
+import com.traderg.cli.Services.BrowserService;
+import com.traderg.cli.Services.HttpListenerService;
 
-import java.awt.Desktop;
-import java.net.URI;
-// move the logic from the backend that takes in the access token and put it in the cli and then let the cli pass an access token back to the backend
-@SpringBootApplication
+
 public class App {
     public static void main(String[] args) {
-        String auth_url = "https://github.com/login/oauth/authorize?client_id=d095913ff9b55112e726&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fplayer%2Fsignin&scope=user+user:email";
         if (args.length > 0 && args[0].equalsIgnoreCase("login")) {
-            try {
-                Desktop.getDesktop().browse(new URI(auth_url));
-            } catch (Exception e) {
-                System.out.println("Failed to open browser for login.");
+            String authUrl = "https://github.com/login/oauth/authorize?client_id=d095913ff9b55112e726&redirect_uri=http://localhost:8888/signin&scope=user";
+
+            BrowserService browserService = new BrowserService();
+            browserService.openBrowser(authUrl);
+
+            HttpListenerService httpListenerService = new HttpListenerService();
+            String code = httpListenerService.startServerAndWaitForCode();
+
+            if (code != null) {
+                BackendService backendService = new BackendService();
+                backendService.sendCodeToBackend(code);
+            } else {
+                System.out.println("Failed to receive the authorization code.");
             }
         }
-
-
     }
 }
