3ed42106b90333d93a352e1d33752ac4d570d5b7
= <Batandwa.Mgutsi@bbd.co.za>
2024-03-14 18:32:26 +0200

    CLI release cycle prep

---
+++ cli/src/main/java/com/traderg/cli/services/BackendService.java
@@ -0,0 +1,61 @@
+package com.traderg.cli.services;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.traderg.cli.backend_models.PlayerWithToken;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class BackendService {
+    final EnvironmentsService environmentsService;
+
+    final HttpClient client = HttpClient.newHttpClient();
+
+    final Gson gson = new Gson();
+
+    public BackendService(EnvironmentsService environmentsService) {
+        this.environmentsService = environmentsService;
+    }
+
+    private HttpRequest.Builder startJsonRequest(String path) {
+        return HttpRequest.newBuilder().uri(
+                URI.create(String.format("%s%s", environmentsService.serverHost, path)))
+                .header("Content-Type", "application/json");
+    }
+
+    private String bodyAsString(HttpResponse<String> response) throws HttpException {
+        if (response.statusCode() >= 200 && response.statusCode() < 300) {
+            return response.body();
+        } else {
+            throw new HttpException(response.body());
+        }
+    }
+
+    private String asJsonString(Map<String, Object> json) {
+        return gson.toJson(json);
+    }
+
+    public PlayerWithToken sendCodeToBackend(String code) throws HttpException, IOException, InterruptedException {
+        final Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("code", code);
+
+        HttpRequest request = startJsonRequest("/player")
+                .PUT(HttpRequest.BodyPublishers.ofString(asJsonString(requestBody)))
+                .build();
+
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return gson.fromJson(bodyAsString(response), PlayerWithToken.class);
+    }
+
+    public static class HttpException extends Exception {
+        HttpException(String message) {
+            super(message);
+        }
+    }
+}
