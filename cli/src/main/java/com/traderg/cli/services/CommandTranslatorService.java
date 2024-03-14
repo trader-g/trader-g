ce86750609177cf126e9b3cfffa0402501627635
KeeganOReilly <keegan.oreilly@bbd.co.za>
2024-03-14 23:22:34 +0200

    Added function headers and translateCommand "interface"

--- cli/src/main/java/com/traderg/cli/services/CommandTranslatorService.java
+++ cli/src/main/java/com/traderg/cli/services/CommandTranslatorService.java
@@ -12,46 +12,59 @@ public class CommandTranslatorService {
     public int playerID;
     public String authKey;
     HttpRequestHandler rH = new HttpRequestHandler();
+    private String previousCommand = "";
 
     public void translateCommand(String command) {
 
-        String words = command.toLowerCase();
+        String words = fixMultipleSpaces(command.toLowerCase());
         String[] wordArr = command.toLowerCase().split("\\s+");
 
-        if (wordArr.length < 3) {
+        if (!words.equals(previousCommand) && words.length() > 0) {
             switch (words) {
-                case "open inventory":
-                    openInventory();
+                case "view inventory":
+                    viewInventory();
                     break;
-                case "open marketplace":
-                    openMarketplace();
+                case "view leaderboard":
+                    viewLeaderboard();
                     break;
-                case "open trade":
-                    openTrade();
+                case "view offers":
+                    viewOffers();
                     break;
-                case "open player-list":
-                    openPlayerList();
+                case "make offer":
+                    makeOffer();
+                    break;
+                case "make trade":
+                    makeTrade();
                     break;
                 case "logout":
                     logout();
                     break;
                 default:
-                    System.out.println("Invalid command:  " + words);
-            }
-        } else {
-            switch (words) {
-                case "make offer":
-                    makeOffer(wordArr[2]);
-                    break;
-                case "make trade":
-                    makeTrade(wordArr[2]);
-                    break;
-            }
-        }
+                    if (wordArr[0].equals("exchange")) {
+                        if(previousCommand.equals("make offer")){
+                            makeExchangeOffer(wordArr);
+                        }
+                        else System.out.println("Invalid command. Can only create a potential exchange after the make offer command");
+                    } 
+                    else if (isInteger(wordArr[0])) {
+                        if(previousCommand.equals("make trade")){
+                            makeTradeHappen(Integer.parseInt(wordArr[0]));
+                        }
+                        else System.out.println("Invalid command. Can only use an id after the make trade command");
+                        
+                        makeTrade();
+                    }
+                    
+                    else {
+                        System.out.println("Invalid command:  " + words);
+                    }
 
+            }
+            previousCommand = words;
+        }
     }
 
-    private void openInventory() {
+    private void viewInventory() {
         // draft up http request
         // make http request to localhost:8080/inventory
         // receive response back from http
@@ -60,53 +73,71 @@ public class CommandTranslatorService {
 
         System.out.println("Inventory functionality...");
 
-        rH.testFunc();
+        // rH.testFunc();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/inventory"))
-                .header("Authorization", "Bearer " + authKey)
-                .build();
+        // HttpClient client = HttpClient.newHttpClient();
+        // HttpRequest request = HttpRequest.newBuilder()
+        //         .uri(URI.create("http://localhost:8080/inventory"))
+        //         .header("Authorization", "Bearer " + authKey)
+        //         .build();
 
-        HttpResponse<String> response;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response from backend: " + response.body());
-            int statusCode = response.statusCode();
-            if (statusCode == 200) {
-                System.out.println(response.body());
+        // HttpResponse<String> response;
+        // try {
+        //     response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //     System.out.println("Response from backend: " + response.body());
+        //     int statusCode = response.statusCode();
+        //     if (statusCode == 200) {
+        //         System.out.println(response.body());
 
-                // do something with this response body, lmao
-            } else {
-                throw new Exception("Failed to fetch inventory data. Response code: " + statusCode);
-            }
-        } catch (Exception e) {
-            System.out.println("Failed to send code to backend: " + e.getMessage());
-        }
+        //         // do something with this response body, lmao
+        //     } else {
+        //         throw new Exception("Failed to fetch inventory data. Response code: " + statusCode);
+        //     }
+        // } catch (Exception e) {
+        //     System.out.println("Failed to send code to backend: " + e.getMessage());
+        // }
 
     }
 
-    private void openMarketplace() {
-        System.out.println("Marketplace functionality...");
+    private void viewLeaderboard() {
+        System.out.println("Leaderboard functionality...");
     }
 
-    private void openTrade() {
+    private void viewOffers() {
         System.out.println("trade open functionality...");
+    }
+
+    private void makeTrade() {
+        System.out.println("make trade functionality...");
+    }
+
+    private void makeOffer() {
+        System.out.println("make offer functionality...");
     }
 
     private void logout() {
         System.out.println("logout functionality...");
     }
 
-    private void makeTrade(String tradeID) {
-        System.out.println("make trade functionality...");
+    private void makeExchangeOffer(String[] potentialExchange) {
+        System.out.println("makeExchangeOffer functionality...");
     }
 
-    private void openPlayerList() {
-
+    private void makeTradeHappen(int tradeID) {
+        System.out.println("makeTradeHappen functionality...");
     }
 
-    private void makeOffer(String offerID) {
+    private boolean isInteger(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private String fixMultipleSpaces(String cmd){
+        return cmd.replaceAll("\\s+", " ");
     }
 
 }
