0e01596de2246d8b2f36bc34675599a2179e6124
batandwa-bbd <156436448+batandwa-bbd@users.noreply.github.com>
2024-03-15 09:14:09 +0200

    Bring back local db (#28)
    * Show inventory
    
    * use add player
    
    * Show inventory
    
    * Bring back local db
    
    ---------
    
    Co-authored-by: GerniVisser-bbd <Gerrit.Visser@bbd.co.za>

--- beancards/src/main/java/com/bbdgrads/beancards/controllers/PlayerController.java
+++ beancards/src/main/java/com/bbdgrads/beancards/controllers/PlayerController.java
@@ -1,5 +1,7 @@
 package com.bbdgrads.beancards.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
---
+++ cli/src/main/java/com/traderg/cli/backend_models/Card.java
@@ -0,0 +1,46 @@
+package com.traderg.cli.backend_models;
+
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class Card {
+    private int id;
+    private String type;
+    private String size;
+
+    public Card(int id, String type, String size) {
+        this.id = id;
+        this.type = type;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public static Card fromJson(Map<String, Object> json) {
+        final Gson gson = new Gson();
+        return gson.fromJson(gson.toJson(json), Card.class);
+    }
+}
